6b41cb55a3404f2cd1d4ab5579c27dfaf624f9b5
Santiago Altea <teya_016@yahoo.com.ph>
2017-02-10 17:11:23 +0800

    fixed timesheet login

--- wp-content/themes/Avada/custom_page_timesheet.php
+++ wp-content/themes/Avada/custom_page_timesheet.php
@@ -1850,8 +1850,26 @@ endif;
 </div>
 <?php
 	else:
-	echo "<div class='login_box'>";
-	if  (function_exists ('wplb_login'))   { wplb_login(); } 
+	echo "<div id='login_box' class='login_box'>";
+
+	// if  (function_exists ('wplb_login'))   { wplb_login(); } 
+		$args = array(
+		        'echo' => true,
+		        'redirect' => site_url( $_SERVER['REQUEST_URI'] ), 
+		        'form_id' => 'loginform',
+		        'label_username' => __( 'Username' ),
+		        'label_password' => __( 'Password' ),
+		        'label_remember' => __( 'Remember Me' ),
+		        'label_log_in' => __( 'Log In' ),
+		        'id_username' => 'user_login',
+		        'id_password' => 'user_pass',
+		        'id_remember' => 'rememberme',
+		        'id_submit' => 'wp-submit',
+		        'remember' => true,
+		        'value_username' => NULL,
+		        'value_remember' => false );
+
+		wp_login_form( $args );
 	echo "</div>";
 	endif; 
 ?>
